44f61aa8959ca8699366c50920238d1288d2694d
Raunak <raunak51299@gmail.com>
2023-03-09 21:55:16 +0530

    added dark mode fnc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import NavBar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -7,7 +8,8 @@ function App() {
     <>
       <NavBar title="TextUtils" aboutText="About" />
       <div className="container my-3">
-        <TextForm title="Enter text below" />
+        {/* <TextForm title="Enter text below" /> */}
+        <About />
       </div>
     </>
   );
