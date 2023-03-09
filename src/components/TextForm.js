0203ef052f97bc6d7b945e035fb6e275c65351f2
Raunak <raunak51299@gmail.com>
2023-03-09 20:15:53 +0530

    added characters and words length

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-
-    const handelOnChange = (event)=>{
-        // console.log("On change");
-        setText(event.target.value);
-    }
-    const handleUpClick = ()=>{
-        // console.log("uppercase clicked");
-        let nText = text.toUpperCase();
-        setText(nText);
-    }
-
-    const [text, setText] = useState('Enter text here');
-    return (
+  const handelOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+  const handleUpClick = () => {
+    // console.log("uppercase clicked");
+    let nText = text.toUpperCase();
+    setText(nText);
+  };
+  const [text, setText] = useState("Enter text here");
+  return (
     <>
-      <h1>{props.title}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handelOnChange}></textarea>
+      <div className="container">
+        <h1>{props.title}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handelOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+      <div className="container my-2">
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{text.split(" ").length * 0.2} seconds needed to read this</p>
+      </div>
     </>
   );
 }
